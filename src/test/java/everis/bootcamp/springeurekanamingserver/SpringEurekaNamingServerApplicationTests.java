e8635f7db33c38fd1d29a406edb4cb7f40d1a357
Joselon <joselon1999@hotmail.com>
2020-03-19 22:18:44 -0500

    first commit

---
+++ src/test/java/everis/bootcamp/springeurekanamingserver/SpringEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package everis.bootcamp.springeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
